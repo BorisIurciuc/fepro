4075442ae761af4f3ac98bc8b28ca4295124a7b3
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-06-16 10:29:08 +0200

    add 2 input fields

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -7,16 +7,19 @@ function LoginForm() {
             <form type='submit' className="form">
             <h3>Login form</h3>
                 <div className="loginForm">
-                    
-                <Input 
-                    name={'name'} 
-                    type={'text'} 
-                    placeholder={'enter your name'} 
-                    label={'userName'}/>
-                <MyButton 
-                    name={'Send data'} 
-                    type={'submit'}/> 
+                    <div className="inputForm">
+                        <Input name={'name'} type={'text'} placeholder={'your name'} label={'userName'}/>
+                        <MyButton name={'Send name'} type={'submit'}/>
                     </div>
+                    <div className="inputForm">
+                        <Input name={'email'} type={'text'} placeholder={'your email'} label={'uselEmail'}/>
+                        <MyButton name={'Send email'} type={'submit'}/>
+                    </div>
+                    <div className="inputForm">
+                        <Input name={'password'} type={'text'} placeholder={'your password'} label={'uselPasswd'}/>
+                        <MyButton name={'Send password'} type={'submit'}/>
+                    </div>
+                </div>
             </form>
         </div>
     )
